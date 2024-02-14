b3d45c6814da59d32dd6a7d2d956cddaee01183e
Atthakon <earthbmza@gmail.com>
2024-02-14 16:35:01 +0700

    abc

--- pokemon.py
+++ pokemon.py
@@ -1,4 +1,84 @@
-import requests
-from PIL import Image
-import io
-# conda install requests pillow 
+##import streamlit as st 
+##from PIL import Image
+##img=Image.open('pokemon_bulbasaur.png')
+##st.image(img)
+import streamlit as st
+st.image('pokemon1-20/pokemon_bulbasaur.png', caption='1.Fushigidane', width=150) #1
+
+st.image('pokemon1-20/pokemon_Fushigisou.png', caption='2.Fushigisou', width=155) #2
+
+st.image('pokemon1-20/pokemon_Fushigibana.png', caption='3.Fushigibana', width=150) #3
+
+st.image('pokemon1-20/pokemon_Hitokage.png', caption='4.Hitokage', width=150) #4
+
+st.image('pokemon1-20/pokemon_Lizardo.png', caption='5.Lizardo', width=150) #5
+
+st.image('pokemon1-20/pokemon_Lizardon.png', caption='6.Lizardon', width=150) #6
+
+st.image('pokemon1-20/pokemon_Zenigame.png', caption='7.Zenigame', width=150) #7
+
+st.image('pokemon1-20/pokemon_Kameil.png', caption='8.Kameil', width=150) #8
+
+st.image('pokemon1-20/pokemon_Kamex.png', caption='9.Kamex', width=150) #9 
+
+st.image('pokemon1-20/pokemon_Caterpie.png', caption='10.Caterpie', width=150) #10
+
+st.image('pokemon1-20/pokemon_Transel.png', caption='11.Transel', width=150) #11
+
+st.image('pokemon1-20/pokemon_Butterfree.png', caption='12.Butterfree', width=150) #12
+
+st.image('pokemon1-20/pokemon_Beedle.png', caption='13.Beedle', width=150) #13 
+
+st.image('pokemon1-20/pokemon_Cocoon.png', caption='14.Cocoon', width=150) #14
+
+st.image('pokemon1-20/pokemon_Spear.png', caption='15.Spear', width=150) #15 
+
+st.image('pokemon1-20/pokemon_Poppo.png', caption='16.Poppo', width=150) #16 
+
+st.image('pokemon1-20/pokemon_Pigeon.png', caption='17.Pigeon', width=150) #17
+
+st.image('pokemon1-20/pokemon_Pigeot.png', caption='18.Pigeot', width=150) #18
+
+st.image('pokemon1-20/pokemon_Koratta.png', caption='19.Koratta', width=150) #19 
+
+st.image('pokemon1-20/pokemon_Ratta.png', caption='20.Ratta', width=150) #20 
+
+st.image('pokemon21-40/pokemon_Onisuzume.png', caption='21.Onisuzume', width=150) #21 
+
+st.image('pokemon21-40/pokemon_Onidrill.png', caption='22.Onidrill', width=150) #22 
+
+st.image('pokemon21-40/pokemon_Arbo.png', caption='23.Arbo', width=150) #23 
+
+st.image('pokemon21-40/pokemon_Arbok.png', caption='24.Arbok', width=150) #24
+
+st.image('pokemon21-40/pokemon_Pikachu.png', caption='25.Pikachu', width=150) #25 
+
+st.image('pokemon21-40/pokemon_Raichu.png', caption='26.Raichu', width=150) #26 
+
+st.image('pokemon21-40/pokemon_Sand.png', caption='27.Sand', width=150) #27
+
+st.image('pokemon21-40/pokemon_Sandpan.png', caption='28.Sandpan', width=150) #28 
+
+st.image('pokemon21-40/pokemon_Nidoran.png', caption='29.Nidoran(Women)', width=150) #29
+
+st.image('pokemon21-40/pokemon_Nidorina.png', caption='30.Nidorina(Women)', width=150) #30
+
+st.image('pokemon21-40/pokemon_Nidoqueen.png', caption='31.Nidoqueen(Women)', width=150) #31
+
+st.image('pokemon21-40/pokemon_Nidoran(men).png', caption='32.Nidoran(men)', width=150) #32
+
+st.image('pokemon21-40/pokemon_Nidorino.png', caption='33.Nidorino(men)', width=150) #33
+
+st.image('pokemon21-40/pokemon_Nidoking.png', caption='34.Nidoking(men)', width=150) #34 
+
+st.image('pokemon21-40/pokemon_Pippi.png', caption='35.Pippi', width=150) #35
+
+st.image('pokemon21-40/pokemon_Pixy.png', caption='36.Pixy', width=150) #36
+
+st.image('pokemon21-40/pokemon_Rokon.png', caption='37.Rokon', width=150) #37
+
+st.image('pokemon21-40/pokemon_Kyukon.png', caption='38.Kyukon', width=150) #38
+
+st.image('pokemon21-40/pokemon_Purin.png', caption='39.Purin', width=150) #39
+
+st.image('pokemon21-40/pokemon_Pukurin.png', caption='40.Pukurin', width=150) #40
