e8b5216f18ccb973f05f684579b0033697ea329b
Atthakon <earthbmza@gmail.com>
2024-02-21 13:58:24 +0700

    asd

--- pokemon.py
+++ pokemon.py
@@ -82,3 +82,89 @@ st.image('pokemon21-40/pokemon_Kyukon.png', caption='38.Kyukon', width=150) #38
 st.image('pokemon21-40/pokemon_Purin.png', caption='39.Purin', width=150) #39
 
 st.image('pokemon21-40/pokemon_Pukurin.png', caption='40.Pukurin', width=150) #40
+
+st.image('pokemon41-60/pokemon_Zubat.png', caption='41.Zubat', width=150) #41
+
+st.image('pokemon41-60/pokemon_Golbat.png', caption='42.Golbat', width=150) #42
+
+st.image('pokemon41-60/pokemon_Nazonokusa.png', caption='43.Nazonokusa', width=150) #43
+
+st.image('pokemon41-60/pokemon_Kusaihana.png', caption='44.Kusaihana', width=150) #44
+
+st.image('pokemon41-60/pokemon_Ruffresia.png', caption='45.Ruffresia', width=150) #45
+
+st.image('pokemon41-60/pokemon_Paras.png', caption='46.Paras', width=150) #46
+
+st.image('pokemon41-60/pokemon_Parasect.png', caption='47.Parasect', width=150) #47
+
+st.image('pokemon41-60/pokemon_Kongpang.png', caption='48.Kongpang', width=150) #48
+
+st.image('pokemon41-60/pokemon_Morphon.png', caption='49.Morphon', width=150) #49
+
+st.image('pokemon41-60/pokemon_Digda.png', caption='50.Digda', width=150) #50
+
+st.image('pokemon41-60/pokemon_Dugtrio.png', caption='51.Dugtrio', width=150) #51
+
+st.image('pokemon41-60/pokemon_Nyarth.png', caption='52.Nyarth', width=150) #52
+
+st.image('pokemon41-60/pokemon_Persian.png', caption='53.Persian', width=150) #53
+
+st.image('pokemon41-60/pokemon_Koduck.png', caption='54.Koduck', width=150) #54
+
+st.image('pokemon41-60/pokemon_Golduck.png', caption='55.Golduck', width=150) #55
+
+st.image('pokemon41-60/pokemon_Mankey.png', caption='56.Mankey', width=150) #56
+
+st.image('pokemon41-60/pokemon_Okorizaru.png', caption='57.Okorizaru', width=150) #57
+
+st.image('pokemon41-60/pokemon_Gardie.png', caption='58.Gardie', width=150) #58
+
+st.image('pokemon41-60/pokemon_Windie.png', caption='59.Windie', width=150) #59
+
+st.image('pokemon41-60/pokemon_Nyoromo.png', caption='60.Nyoromo', width=150) #60
+
+st.image('pokemon61-80/pokemon_Nyorozo.png', caption='61.Nyorozo', width=150) #61
+
+st.image('pokemon61-80/pokemon_Nyorobon.png', caption='62.Nyorobon', width=150) #62
+
+st.image('pokemon61-80/pokemon_Casey.png', caption='63.Casey', width=150) #63
+
+st.image('pokemon61-80/pokemon_Yungerer.png', caption='64.Yungerer', width=150) #64
+
+st.image('pokemon61-80/pokemon_Foodin.png', caption='65.Foodin', width=150) #65
+
+st.image('pokemon61-80/pokemon_Wanriky.png', caption='66.Wanriky', width=150) #66
+
+st.image('pokemon61-80/pokemon_Goriky.png', caption='67.Goriky', width=150) #67
+
+st.image('pokemon61-80/pokemon_Kairiky.png', caption='68.Kairiky', width=150) #68
+
+st.image('pokemon61-80/pokemon_Madatsubomi.png', caption='69.Madatsubomi', width=150) #69
+
+st.image('pokemon61-80/pokemon_Utsudon.png', caption='70.Utsudon', width=150) #70
+
+st.image('pokemon61-80/pokemon_Utsubot.png', caption='71.Utsubot', width=150) #71
+
+st.image('pokemon61-80/pokemon_Menokurage.png', caption='72.Menokurage', width=150) #72
+
+st.image('pokemon61-80/pokemon_Dokukurage.png', caption='73.Dokukurage', width=150) #73
+
+st.image('pokemon61-80/pokemon_Isitsubute.png', caption='74.Isitsubute', width=150) #74
+
+st.image('pokemon61-80/pokemon_Golone.png', caption='75.Golone', width=150) #75
+
+st.image('pokemon61-80/pokemon_Golonya.png', caption='76.Golonya', width=150) #76
+
+st.image('pokemon61-80/pokemon_Ponyta.png', caption='77.Ponyta', width=150) #77
+
+st.image('pokemon61-80/pokemon_Gallop.png', caption='78.Gallop', width=150) #78
+
+st.image('pokemon61-80/pokemon_Yadon.png', caption='79.Yadon', width=150) #79
+
+st.image('pokemon61-80/pokemon_Yadoran.png', caption='80.Yadoran', width=150) #80
+
+st.image('pokemon81-100/pokemon_Coil.png', caption='81.Coil', width=150) #81
+
+st.image('pokemon81-100/pokemon_Rarecoil.png', caption='82.Rarecoil', width=150) #82
+
+st.image('pokemon81-100/pokemon_Kamonegi.png', caption='83.Kamonegi', width=150) #83
